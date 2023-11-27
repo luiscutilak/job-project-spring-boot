bd8781266f3a4886f0fc4b7222f8ecb76a3a2801
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-11-27 16:23:57 -0300

    feat: trabalhando com headers APi DOG

--- src/main/java/br/com/luiscutilak/primeiroprojetospringboot/controller/PrimeiraController.java
+++ src/main/java/br/com/luiscutilak/primeiroprojetospringboot/controller/PrimeiraController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +32,25 @@ public class PrimeiraController {
     }
 
     @PostMapping("/metodoComBodyParams")
-    public String metodoComBodyParams(@RequestBody String username) {
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
 
-        return "metodoComBodyParams" + username;
+        return "metodoComBodyParams " + usuario.username();
     }
 
-    
+    record Usuario(String username) {
+
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader ("name")String name) {
+
+        return "metodo Com Headers " + name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+
+        return "metodo Com List Headers " + headers.entrySet();
+    }
+
 }
