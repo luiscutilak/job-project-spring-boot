9fc23456d738175aa51ef166ecf956a232213484
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-11-27 11:03:42 -0300

    feat: initializing project

---
+++ src/main/java/br/com/luiscutilak/primeiroprojetospringboot/PrimeiroProjetoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.luiscutilak.primeiroprojetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luiscutilak/primeiroprojetospringboot/PrimeiroProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiscutilak.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
